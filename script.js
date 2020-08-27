bde3dd9bdb2fe4cc3614f004849261790f1bb84c
JoaoAlvesJF <JoaoAlves.JFAL@outlook.com>
2020-08-27 01:09:45 -0300

    Removendo todo e salvando na storage

--- script.js
+++ script.js
@@ -2,20 +2,26 @@ var listElement = document.querySelector('ul');
 var inputElement = document.querySelector('input');
 var buttonElement = document.querySelector('button');
 
-var todos = [
-    'Fazer Café',
-    'Estudar JS',
-    'Estudar VueJS',
-];
+var todos = JSON.parse(localStorage.getItem('listTodos')) || [];
 
 function renderTodos(){
     listElement.innerHTML = '';
     for(todo of todos) {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
+        var linkElement = document.createElement('a');
+        var linkText = document.createTextNode('excluir');
 
+        var pos = todos.indexOf(todo)
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')')
+
+        linkElement.setAttribute('href', '#')
+
+        linkElement.appendChild(linkText);
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
         listElement.appendChild(todoElement);
+        
     }
 
 };
@@ -27,6 +33,18 @@ function addTodo(){
     todos.push(todoText)
     inputElement.value = ''
     renderTodos()
+    saveToStorage()
 };
+
+function deleteTodo(pos){
+    todos.splice(pos, 1)
+    renderTodos()
+    saveToStorage()
+}
+
+function saveToStorage(){
+    localStorage.setItem('listTodos', JSON.stringify(todos))
+
+}
 
 buttonElement.onclick = addTodo;
